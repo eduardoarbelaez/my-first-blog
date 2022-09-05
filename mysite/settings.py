3ca2542598f4eb7dcb97dae827917ea2107a73a9
eduardoarbelaez <eduardoarbelaez@hotmail.com>
2022-09-05 09:35:46 -0500

    Agregadas vistas y plantilla para el detalle del post del blog así como también CSS para el sitio.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cg&4&t83tjxkva%$-b^lv(lbcj%(w#y(i!6%funlngsnaesg2^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
